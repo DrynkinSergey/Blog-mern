8cbb35f76346849cc320f9c178b1b848a41f158a
Sergey <sergsmile29@gmail.com>
2023-01-06 02:16:15 +0200

    -create auth -create midleware for check my token -create userController

--- index.js
+++ index.js
@@ -10,7 +10,13 @@ import {validationResult }from 'express-validator'
 
 import {registerValidation} from './validations/auth.js'
 
+
+
 import UserModel from './models/User.js'
+import checkAuth from './utils/checkAuth.js'
+import User from "./models/User.js";
+import * as UserController from "./controllers/UserController.js";
+
 
 mongoose.connect(`${process.env.REACT_APP_API_KEY}`)
     .then(() => console.log('DB ok'))
@@ -21,36 +27,11 @@ const app = express()
 app.use(express.json())
 
 
-app.post('/auth/register', registerValidation, async (req, res) => {
-    try {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()){
-            return res.status(400).json(errors.array())
-        }
+app.post('/auth/login',   UserController.login)
 
-        const password = req.body.password;
-        const salt = await bcrypt.genSalt(10)
-        const passwordHash = await bcrypt.hash(password,salt)
+app.post('/auth/register', registerValidation,  UserController.register)
 
-        const doc = new UserModel({
-            email: req.body.email,
-            fullName:req.body.fullName,
-            avatarUrl:req.body.avatarUrl,
-            passwordHash,
-        })
-
-        const user = await doc.save()
-
-        res.json({
-            success:true
-        })
-    } catch (err){
-        console.log(err)
-        res.status(500).json({
-            message: 'Не удалось зарегистрироваться'
-        })
-    }
-})
+app.get('/auth/me', checkAuth, UserController.getMe)
 
 app.listen(4444, (err) => {
     if (err) {
